844913f046225a3a5c1b06ccccbfbce8380e6e85
Alberto Echeverri <>
2020-03-28 08:03:50 -0600

    Project setup

---
+++ AltimetrikWeather/AltimetrikWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AltimetrikWeather
+//
+//  Created by Alberto Echeverri on 3/28/20.
+//  Copyright © 2020 Alberto Echeverri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
